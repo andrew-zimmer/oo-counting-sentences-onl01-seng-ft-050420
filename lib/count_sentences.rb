8ea4f40a201403a454bc0d8a2b8e9936834edce5
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-12 14:18:33 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,11 +3,11 @@ require 'pry'
 class String
 
   def sentence?
-    self.split(" ").any?{|words|words.end_with?(".")}
+    self.split(" ").any?{|words| words.end_with?(".")}
   end
 
   def question?
-    self.split(" ").any? { |words| words.end_with?("?")}
+    self.split(" ").any? {|words| words.end_with?("?")}
   end
 
   def exclamation?
